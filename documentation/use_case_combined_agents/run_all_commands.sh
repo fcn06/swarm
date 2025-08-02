b1b49c33641bf8d666016fd334333fbf39d0ef70
frederic.nin.ia <218342477+fcn06@users.noreply.github.com>
2025-08-02 07:38:16 +0000

    Adjustments on Multi Agent System Script for lisibility

--- documentation/use_case_combined_agents/run_all_commands.sh
+++ documentation/use_case_combined_agents/run_all_commands.sh
@@ -15,32 +15,36 @@ echo "**************************************************************************
 
 read -n 1 -s -r -p "Press any key to start..."
 
+echo $'\n'
 echo "A) Compile the project"
 cargo build --release 
+echo $'\n'
 
 echo "B) Compile the mcp server example"
 cargo build --release --example main-server
+echo $'\n'
 
 echo "C) Launch three MCP server, each with individual tool on three different ports"
+echo $'\n'
 ./target/release/examples/main-server --port 8001 weather &
 ./target/release/examples/main-server --port 8002 customer &
 ./target/release/examples/main-server --port 8003 scrape &
-
+echo $'\n'
 
 
 echo "D) Launch three agents listening on three different ports (8081,8083,8085), each using mcp config"
 ./target/release/simple_agent_server  --config-file "documentation/use_case_combined_agents/weather_a2a_agent.toml" &
 ./target/release/simple_agent_server  --config-file "documentation/use_case_combined_agents/customer_domain_a2a_agent.toml" &
 ./target/release/simple_agent_server  --config-file "documentation/use_case_combined_agents/web_scraper_a2a_agent.toml" &
-
-read -n 1 -s -r -p "Press any key to continue after three agents are launched..."
-
+echo $'\n' 
+read -n 1 -s -r -p "Press any key to continue after three agents are launched on port 8081,8083,8085..."
+echo $'\n'
 
 echo "E) Launch full agent listening on port 9080, connected to three individual agents"
 ./target/release/full_agent_server --config-file "documentation/use_case_combined_agents/agent_full_config.toml" &
-
-read -n 1 -s -r -p "Press any key to continue after three agents are launched..."
-
+echo $'\n'
+read -n 1 -s -r -p "Press any key to continue after coordinator agent is launched..."
+echo $'\n'
 
 
 echo "F) Launch the a2a_client with 4 requests, three of them are targetting a specific tool, and the fourth a general knowledge question"
@@ -57,5 +61,6 @@ echo "F) Launch the a2a_client with 4 requests, three of them are targetting a s
 #read -n 1 -s -r -p "Press any key to continue..."
 
 echo "Ask the questions to full agent, the orchestrator one : He should answer to the whole set of questions"
+echo $'\n'
 ./target/release/simple_a2a_client --port 9080 
-
+echo $'\n'

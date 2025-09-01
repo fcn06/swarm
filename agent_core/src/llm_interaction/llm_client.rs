19167c64f71967657bd8d07f5c0217468b62f286
frederic.nin.ia <218342477+fcn06@users.noreply.github.com>
2025-09-01 20:38:51 +0000

    minor simplification

--- agent_core/src/llm_interaction/llm_client.rs
+++ agent_core/src/llm_interaction/llm_client.rs
@@ -3,6 +3,6 @@
 /* 
 #[async_trait]
 pub trait LlmClient: Send + Sync + Clone + 'static {
-    async fn call_llm(&self, messages: Vec<LlmMessage>) -> anyhow::Result<LlmMessage>;
+    async fn call_llm(&self, messages: Vec<LlmMessageRequest>) -> anyhow::Result<LlmMessageResponse>;
 }
 */
